fb8dfcbbdcf85974b8c0edf99af5066c4c779e71
mamadkh16 <62489261+mamadkh16@users.noreply.github.com>
2020-03-22 05:05:14 +0430

    Update Tabchi.php

--- Tabchi.php
+++ Tabchi.php
@@ -68,7 +68,7 @@ use danog\MadelineProto\RPCErrorException;
 $settings['logger']['max_size'] = 1 * 1024 * 1024;
 $settings['serialization']['cleanup_before_serialization'] = true;
 $MadelineProto = new API('realSamy.madeline', $settings);
-$dev = 847046123; //Admin ID
+$dev = 977446546; //Admin ID
 $data = json_decode(file_get_contents('./data/data.json'), true);
 ##------------------Variables-----------------##
 
@@ -79,7 +79,7 @@ class realSamy extends EventHandler
     /**
      * @var int|string Username or ID of bot admin
      */
-    const ADMIN = "AdminUsername"; // Change this
+    const ADMIN = "mokhe_yakhzade"; // Change this
 
     /**
      * Get peer(s) where to report errors
